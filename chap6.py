858ab3890c310a9d176a9b49bb2b1919eaea292c
StephanieFrias <stephanie.frias@gmail.com>
2013-07-07 14:01:32 -0400

    dev-sprint2

--- chap6.py
+++ chap6.py
@@ -1,9 +1,42 @@
-# Enter your answrs for chapter 6 here
-# Name:
+# Enter your answers for chapter 6 here
+# Name: Stephanie Frias
 
 
 # Ex. 6.6
+def first(word):
+	return word[0]
+	
+def last(word):
+	return word[-1]
 
+def middle(word):
+	return word[1:-1]
+	
+def is_palindrome(word):
+		if len(word)<=1:
+			return True
+		if first(word) != last(word):
+			return False
+		return is_palindrome(middle(word))
 
+print is_palindrome('Stephanie')
+print is_palindrome('redivider')
+print is_palindrome('racecar')
+print is_palindrome('poop')
 
 # Ex 6.8
+
+def gcd(a,b):
+	c = abs (a-b)
+	if a == b: 
+		return b
+	if a > b:
+		return gcd(c,b)
+	if a < b:
+		return gcd(c,a)
+
+print gcd(11,7)
+print gcd(120,36)
+print gcd(300,456)
+
+
--- chap7.py
+++ chap7.py
@@ -1,9 +1,34 @@
-# Enter your answrs for chapter 7 here
-# Name:
+# Enter your answers for chapter 7 here
+# Name: Stephanie Frias
 
 
 # Ex. 7.5
 
+import math
+	
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		recurse = factorial(n-1)
+		result = n * recurse
+		return result		
 
+def estimate_pi():
+	s = 0
+	summation = 0
+	while True:
+		multiplier = ((2*math.sqrt(2))/9801)
+		numerator = ((factorial(4*s))*(1103+26390*s))
+		denominator =((factorial(s))**4)*(396**(4*s))
+		calculation = multiplier*(numerator/denominator)
+		summation += calculation
+
+		if abs(calculation) <1e-15: break
+		s += 1
+
+	return 1/summation
+
+print estimate_pi()
 
 # How many iterations does it take to converge?
